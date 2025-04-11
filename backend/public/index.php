ecfe551334c5d86360fb5183ca721cf4234e4030
El-Abdo <abdulrahman.muhammad2090@gmail.com>
2025-04-11 20:02:10 +0200

    Add CORS headers for API access

--- backend/public/index.php
+++ backend/public/index.php
@@ -1,5 +1,14 @@
 <?php
 
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Headers: Content-Type');
+header('Access-Control-Allow-Methods: POST, OPTIONS');
+
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    http_response_code(204);
+    exit();
+}
+
 require __DIR__ . '/../vendor/autoload.php';
 
 use Abdelrahman\Backend\Config\DB;
